a8047c7b646ec4b1e70c7b3d346026e6084d6c45
Wei Mo <miloweimo@outlook.com>
2024-01-18 17:20:36 +0800

    refactor: re init
    Signed-off-by: Wei Mo <miloweimo@outlook.com>

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,12 +6,14 @@ module.exports = {
   'extends': [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
+    '@vue/eslint-config-typescript',
     '@vue/eslint-config-prettier/skip-formatting'
   ],
   overrides: [
     {
       files: [
-        'cypress/e2e/**/*.{cy,spec}.{js,ts,jsx,tsx}'
+        'cypress/e2e/**/*.{cy,spec}.{js,ts,jsx,tsx}',
+        'cypress/support/**/*.{js,ts,jsx,tsx}'
       ],
       'extends': [
         'plugin:cypress/recommended'
